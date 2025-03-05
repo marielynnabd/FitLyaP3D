d89038aa7219a952ad8f85c0223995b6e1d8cd37
marielynnabd <marie-lynn.abdulkarim@cea.fr>
2025-03-05 03:41:06 -0800

    Bug fix in likelihood function

--- Forestflow/chi_square_cost.py
+++ Forestflow/chi_square_cost.py
@@ -79,20 +79,24 @@ def vary_params(minuit_object, varying_params_keys):
 class Likelihood_Pcross:
     """ Likelihood cost function used in Minuit minimization """
 
-    def __init__(self, model, y_data, y_err, minimize_chi_square=True, verbose=0):
+    def __init__(self, model, y_data, y_err, minimize_chi_square=True, return_model=False, verbose=0):
         """
         model: function that takes (mF, T0, gamma, lambda_pressure) and returns predicted Px
         y_data: measured Px values
         y_err: errors on Px
+        return_model: this must be false when running Iminuit
         """
         self.model = model  # The wrapped model function
         self.y_data = np.asarray(y_data)
         self.y_err = np.asarray(y_err)
         self.minimize_chi_square = minimize_chi_square
+        self.return_model = return_model
         self.verbose = verbose
 
         # Extract parameter names automatically
         self.parameters = describe(model)  # Only free parameters remain
+        print(self.parameters)
+        print(self.model)
 
     def __call__(self, mF, T0, gamma, lambda_pressure):
         """ Computes chi-square or log-likelihood given parameter values """
@@ -104,7 +108,15 @@ class Likelihood_Pcross:
         chi_square = np.sum((self.y_data - y_model) ** 2 / self.y_err ** 2)
 
         if self.minimize_chi_square:
-            return y_model, chi_square
+            if self.return_model:
+                return y_model, chi_square
+            else:
+                return chi_square
         else:
-            return y_model, -0.5 * chi_square  # Log-likelihood
+            if self.return_model:
+                return y_model, -0.5 * chi_square  # Log-likelihood
+            else:
+                return  -0.5 * chi_square
+
+
 
